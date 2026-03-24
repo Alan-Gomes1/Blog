a07bca9077fb845c42a73e548e6a4d5321960437
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-03-23 22:55:37 -0300

    feat: Adds header component to homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Container } from "../components/Container";
+import Header from "../components/Header";
 import { SpinLoader } from "../components/SpinLoader";
 
 export default async function HomePage() {
   return (
     <Container>
+      < Header />
       <SpinLoader className="min-h-screen" />
     </Container>
   );
