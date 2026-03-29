d06df5875e3d393a6165d842c581fb6ea581e030
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-03-29 20:24:57 -0300

    fix: Enhances homepage with post cover and async posts list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
 import { Container } from "../components/Container";
 import Header from "../components/Header";
 import { SpinLoader } from "../components/SpinLoader";
+import { PostCoverImage } from "../components/PostCoverImage";
+import { Suspense } from "react";
+import { PostsList } from "../components/PostsList";
 
 export default async function HomePage() {
   return (
     <Container>
-      < Header />
-      <SpinLoader className="min-h-screen" />
+      <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <PostCoverImage
+          linkProps={{ href: "#" }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_9.png",
+            alt: "Post Cover Image",
+            priority: true,
+          }}
+        />
+      </section>
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
     </Container>
   );
 }
