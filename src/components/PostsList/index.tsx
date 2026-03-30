e47544a076fa28ae68a5696ed67daa8cea5c9412
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-03-30 17:56:40 -0300

    fix: Refactors post link construction for consistency

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -8,12 +8,12 @@ export async function PostsList() {
   return (
     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => {
+        const postLink = `/post/${post.slug}`;
+
         return (
           <div key={post.id} className="flex flex-col gap-4 group">
             <PostCoverImage
-              linkProps={{
-                href: `/post/${post.slug}`,
-              }}
+              linkProps={{ href: postLink }}
               imageProps={{
                 width: 1200,
                 height: 720,
@@ -29,7 +29,7 @@ export async function PostsList() {
               >
                 {post.createdAt}
               </time>
-              <PostHeading url={`/post/${post.slug}`} as="h1">
+              <PostHeading url={postLink} as="h1">
                 {post.title}
               </PostHeading>
               <p>{post.excerpt}</p>
