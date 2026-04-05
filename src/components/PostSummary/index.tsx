e16a121eb04cd0713d5122186f603f880b6a0c3a
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-04-05 20:20:14 -0300

    feat: Add reusable component for post summary display

---
+++ src/components/PostSummary/index.tsx
@@ -0,0 +1,37 @@
+import {
+  formatDateTime,
+  formatRelativeDate,
+} from "@/src/utils/format-datetime";
+import { PostHeading } from "../PostHeading";
+
+type PostSummaryProps = {
+  postHeading: "h1" | "h2";
+  postLink: string;
+  createdAt: string;
+  title: string;
+  excerpt: string;
+};
+
+export function PostSummary({
+  postHeading,
+  postLink,
+  createdAt,
+  title,
+  excerpt,
+}: PostSummaryProps) {
+  return (
+    <div className="flex flex-col sm:justify-center">
+      <time
+        className="text-slate-600 block text-sm/tight"
+        dateTime={createdAt}
+        title={formatRelativeDate(createdAt)}
+      >
+        {formatDateTime(createdAt)}
+      </time>
+      <PostHeading url={postLink} as={postHeading}>
+        {title}
+      </PostHeading>
+      <p>{excerpt}</p>
+    </div>
+  );
+}
